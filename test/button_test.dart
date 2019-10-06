09827a2e05085df4f10e1d40338e665fa147e83f
danielVg <danielveragi@gmail.com>
2019-10-06 14:35:15 +0200

    uncommented tests for later

--- test/button_test.dart
+++ test/button_test.dart
@@ -1,20 +1,19 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_grpc/widgets/app_button/app_button.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/material.dart';
 
-class TestButton extends StatelessWidget {
+// class TestButton extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      home: AppButton(buttonName: "Test", onPressed: null, buttonTextStyle: null,)
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Flutter Demo',
+//       home: AppButton(buttonName: "Test", onPressed: null, buttonTextStyle: null,)
+//     );
+//   }
+// }
 
-void main() {
-  testWidgets('Test the button', (WidgetTester tester) async {
-    await tester.pumpWidget(TestButton());
-  });
-}
+// void main() {
+//   testWidgets('Test the button', (WidgetTester tester) async {
+//     await tester.pumpWidget(TestButton());
+//   });
+// }
