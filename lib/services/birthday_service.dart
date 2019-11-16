9352116b9502afc333ce2bf9dc9ff86c0e229e26
danielVg <danielveragi@gmail.com>
2019-11-16 20:44:08 +0100

    Changed service to get it working after experimenting

--- lib/services/birthday_service.dart
+++ lib/services/birthday_service.dart
@@ -1,15 +1,31 @@
+import 'package:flutter/rendering.dart';
 import "package:flutter_grpc/common/grpc_commons.dart";
 import "package:flutter_grpc/model/birthday.pb.dart";
 import "package:flutter_grpc/model/birthday.pbgrpc.dart";
 import 'package:fixnum/fixnum.dart';
 
 class BirthdayService {
-  static Future<BirthdayStatus> checkBirthday() async {
-    var client = BirthdayCheckerClient(GrpcClientSingleton().client);
 
-    return await client.checkBirthday(new Date()
-    ..day = Int64(12)
-    ..month = Int64(12)
-    ..year = Int64(1999));
+  static Future<BirthdayStatus> checkBirthday() async {
+    var stub = BirthdayCheckerClient(GrpcClientSingleton().channel);
+
+    try {
+      final response = await stub.checkBirthday(new Date()
+        ..day = 12
+        ..month = 12
+        ..year = 1999);
+      print('Birthday received: ${response.age}');
+      return response;
+    }catch (e){
+      print('Caught error: $e');
+    }
+
+    await GrpcClientSingleton().channel.shutdown();
+    
+//    return await stub.checkBirthday(new Date()
+//    ..day = Int64(12)
+//    ..month = Int64(12)
+//    ..year = Int64(1999));
+
   }
 }
